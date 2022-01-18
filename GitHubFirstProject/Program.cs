a2d8dede48518391ec694a4e2c1c34dfb2ca59ba
Patrick CPH <patrick@klingaa.dk>
2022-01-18 08:44:44 +0100

    8:40 push again

--- GitHubFirstProject/Program.cs
+++ GitHubFirstProject/Program.cs
@@ -1,16 +1,1 @@
-﻿Person p = new("Niels", "Olesen", new DateTime(1992, 4, 5));
-
-Console.WriteLine(p.GetFullName());
-
-Person p2 = new();
-
-Console.WriteLine(p2.GetFullName());
-
-//birthday
-Console.WriteLine(p.birthDateInDanishFormat);
-
-//age
-Console.WriteLine(p.Age);
-
-//timespan
-Console.WriteLine(p.TimeSpanInTotalDays);
+﻿
